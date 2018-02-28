e01573a1674f515fa5993fe5f09257d99782c340
rizha <rizha.musthafa@gmail.com>
2018-02-28 18:44:30 +0700

    add init class and token retrieve

--- cpms/cpms.py
+++ cpms/cpms.py
@@ -1,3 +1,5 @@
+import requests
+from urllib.parse import urlparse, urlencode
 from json import JSONDecodeError
 from requests.exceptions import HTTPError
 
@@ -22,3 +24,54 @@ def validate_response(response):
         raise HTTPError(message)
 
 
+class CpmsConnector:
+    """The CpmsConnector object allow you communicate through
+    cpms between application.
+    """
+
+    ORDER_STATUS = ('NEW', 'IN_PROGRESS', 'COMPLETED', 'CANCELED', 'ERROR')
+
+    def __init__(self, config):
+        """initialize with config
+        config(dict): must supply username, api_key, api_url
+        """
+        self.username = config['username']
+        self.api_key = config['api_key']
+        self.api_url = config['api_url']
+        self._token = None
+        self._set_token()
+
+    @property
+    def _fulfillment_url(self):
+        netloc = f'fulfillment.{urlparse(self.api_url).netloc}'
+        return urlparse(self.api_url)._replace(netloc=netloc).geturl()
+
+    def _update_headers(self, token):
+        self.headers = {
+            'X-Subject-Token': token
+        }
+
+    @property
+    def token(self):
+        return self._token
+
+    def _set_token(self):
+        path = '/identity/token'
+
+        payload = {
+            "auth":
+                {
+                    "apiKeyCredentials":
+                    {
+                        "username": self.username,
+                        "apiKey": self.api_key
+                    }
+                }
+        }
+
+        url = urlparse(self.api_url)._replace(path=path).geturl()
+        r = requests.post(url, json=payload)
+        validate_response(r)
+        token = r.json()['token']['token_id']
+        self._update_headers(token)
+        self._token = token
